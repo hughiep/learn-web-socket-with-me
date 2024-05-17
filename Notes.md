f41241c921f0de3cebf7fb02bfe1a9d85c6abdcd
Hiep <hiepnv.wt@gmail.com>
2024-05-18 00:40:36 +0700

    add simple chat logic

---
+++ Notes.md
@@ -0,0 +1,19 @@
+# Websocket
+
+- Event-driven: Event-driven programming is a programming paradigm in which the flow of the program
+is determined by events such as user actions (mouse clicks, key presses), sensor outputs, or messages from other programs/threads.
+
+- Duplex: A duplex communication system is a point-to-point system composed of two or more connected parties
+or devices that can communicate with one another in both directions.
+
+- Full Duplex: Full-duplex data transmission means that data can be transmitted in both directions on a signal carrier at the same time.
+
+- Bidirectional: Bidirectional communication is a type of communication that occurs in two directions.
+It is a form of communication that allows two different devices to send and receive data.
+
+Compare with Long Polling
+
+## Long pooling
+
+- Keep the connection open/alive until the server has new data to send to the client.
+- When server has new data, the client will receive it and immediately send a new request to the server.
--- simple-chat/server.js
+++ simple-chat/server.js
@@ -1,27 +1,76 @@
-var WebSocketServer = require("ws").Server,
-  wss = new WebSocketServer({ port: 8181 });
+const { v4: uuidv4 } = require("uuid");
+let WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 8181 });
 
-const uuidv4 = require("uuid/v4");
 const clients = [];
 
+let clientIndex = 1;
 wss.on("connection", function (ws) {
-  const clientId = uuidv4();
-  clients.push({ id: clientId, ws });
+  let client_uuid = uuidv4();
+  let nickname = "AnonymousUser" + clientIndex;
+  clients.push({ id: client_uuid, ws: ws, nickname: nickname });
+  console.log("client [%s] connected", client_uuid);
+  clientIndex += 1;
 
-  console.log("client [%s] connected", clientId);
+  let connect_message = nickname + " has connected";
+  wsSend("notification", client_uuid, nickname, connect_message);
 
-  ws.on("message", function (message) {
-    for (var i = 0; i < clients.length; i++) {
-      var clientSocket = clients[i].ws;
-      if (clientSocket.readyState === WebSocket.OPEN) {
-        console.log("client [%s]: %s", clients[i].id, message);
-        clientSocket.send(
-          JSON.stringify({
-            id: clients[i].id,
-            message: message,
-          })
-        );
+  ws.on("message", function message(data, isBinary) {
+    const message = isBinary ? data : data.toString();
+    if (message.indexOf("/nick") === 0) {
+      let nickname_array = message.split(" ");
+      if (nickname_array.length >= 2) {
+        let old_nickname = nickname;
+        nickname = nickname_array[1];
+
+        let nickname_message =
+          "Client " + old_nickname + " changed to " + nickname;
+        wsSend("nick_update", client_uuid, nickname, nickname_message);
       }
+    } else {
+      console.log({ message });
+      wsSend("message", client_uuid, nickname, message);
     }
   });
+
+  let closeSocket = function (customMessage) {
+    for (let i = 0; i < clients.length; i++) {
+      if (clients[i].id == client_uuid) {
+        let disconnect_message;
+        if (customMessage) {
+          disconnect_message = customMessage;
+        } else {
+          disconnect_message = nickname + " has disconnected";
+        }
+        wsSend("notification", client_uuid, nickname, disconnect_message);
+        clients.splice(i, 1);
+      }
+    }
+  };
+
+  ws.on("close", function () {
+    closeSocket();
+  });
+
+  process.on("SIGINT", function () {
+    console.log("Closing things");
+    closeSocket("Server has disconnected");
+    process.exit();
+  });
 });
+
+function wsSend(type, client_uuid, nickname, message) {
+  for (let i = 0; i < clients.length; i++) {
+    let clientSocket = clients[i].ws;
+    if (clientSocket.readyState === WebSocket.OPEN) {
+      clientSocket.send(
+        JSON.stringify({
+          type: type,
+          id: client_uuid,
+          nickname: nickname,
+          message: message,
+        })
+      );
+    }
+  }
+}
